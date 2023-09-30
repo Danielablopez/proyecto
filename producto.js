d1887ee92aea63efe7a54691fb188ad30cfb4e08
DanielaBetianaLopez <prueba@correo.com>
2023-09-30 18:31:00 -0300

    "agregue funcion asincronica"

--- producto.js
+++ producto.js
@@ -1,24 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
     const detalleProducto = document.getElementById("detalle-producto");
   
-    // obtener el ID del producto de la URL
+   
     const urlParams = new URLSearchParams(window.location.search);
     const idProducto = urlParams.get("id");
   
-    // obtenemos los datos de los productos desde el localstorage
+    
     const datosProductos = JSON.parse(localStorage.getItem("productos"));
   
-    //buscamos los productos por id
+    
     const productoSeleccionado = datosProductos.productos.find(
       (producto) => producto.id === idProducto
     );
   
     if (productoSeleccionado) {
-      //creamos el elemento div para mostrar los detalles del producto y lo agregamos
+      
       const contenedorDetallesProducto = document.createElement("div");
       contenedorDetallesProducto.classList.add("detalles-producto");
       contenedorDetallesProducto.innerHTML = `
-      
+
         <img id="imagen-producto" src="${productoSeleccionado.imagen}" alt="${productoSeleccionado.alt}" />
         <h2 id="nombre">${productoSeleccionado.nombre}</h2>
         <p id="descripcion">${productoSeleccionado.descripcion}</p>
--- script.js
+++ script.js
@@ -1,40 +1,46 @@
-fetch("productos.json")
-  .then((respuesta) => respuesta.json())
-  .then((datos) => localStorage.setItem("productos", JSON.stringify(datos)));
+//creo una funcion asincronicaen donde luego de esperar por el fetch devuelvo la variable "datos"
 
+async function getJson() {
+  const respuesta = await fetch("./productos.json");
+  const datos = await respuesta.json();
+  return datos;
+}
+//llamo a la funcion async y con el THEN me aseguro que ya termino de hacer lo que tenia que hacer
+// dentro del THEN pongo todo loque quiero que haga luego con todos los productos!
 document.addEventListener("DOMContentLoaded", () => {
   const gridProductos = document.getElementById("grid-productos");
-  const datosProductos = JSON.parse(localStorage.getItem("productos"));
- 
+  
+  getJson().then((datosProductos) => {
+    
+    if (datosProductos) {
+      localStorage.setItem("productos", JSON.stringify(datosProductos));
+      datosProductos.productos.forEach((producto) => {
+       
+        const gridItem = document.createElement("div");
+        
+        gridItem.classList.add("grid-item");
+       
+        gridItem.innerHTML = `
+          
+    
+          <img src=${producto.imagen} id="imagen" alt="${producto.alt}" />
+          <h4>${producto.nombre}</h4>
+          <h6>${producto.alt}</h6>
+          `;
 
-  if (datosProductos) {
-    datosProductos.productos.forEach((producto) => {
-      // 1- crear uhn div para cada producto
-      const gridItem = document.createElement("div");
-      // 2 agregar la clase nombre al div que contiene el nombre
-      gridItem.classList.add("grid-item");
-      //3- agregar etiqueta h4 con el texto del titulo
-      gridItem.innerHTML = `
-      
+        // gridItem.addEventListener("click", () =>
+        //   mostrarDetallesProductos(producto)
+        // );
 
-      <img src=${producto.imagen} id="imagen" alt="${producto.alt}" />
-      <h4>${producto.nombre}</h4>
-      <h6>${producto.alt}</h6>
-      `;
-      
-      // gridItem.addEventListener("click", () =>
-      //   mostrarDetallesProductos(producto)
-      // );
-
-      gridProductos.appendChild(gridItem);
-      let ver = document.createElement('a');
-      ver.innerText= "Ver Producto";
-      gridItem.append(ver);
-      ver.className= "ver";
-      ver.addEventListener("click", ()=>mostrarDetallesProductos(producto) );
-
-    });
-  }
+        gridProductos.appendChild(gridItem);
+        let ver = document.createElement("a");
+        ver.innerText = "Ver Producto";
+        gridItem.append(ver);
+        ver.className = "ver";
+        ver.addEventListener("click", () => mostrarDetallesProductos(producto));
+      });
+    }
+  });
 });
 
 function mostrarDetallesProductos(producto) {
